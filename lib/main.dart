6295512e208d2ec47a9c757cb4e4874da5a4faba
HazemAttia7 <lolhazem41@gmail.com>
2025-11-02 18:25:56 +0200

    feat: splash view done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pixel_true_app/core/utils/app_colors.dart';
 import 'package:pixel_true_app/core/utils/app_router.dart';
@@ -15,14 +16,23 @@ class MonumentalHabits extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      title: 'Monumental Habits',
-      theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: AppColors.scaffoldColor,
-        textTheme: GoogleFonts.manropeTextTheme(ThemeData.light().textTheme),
-      ),
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, widget) {
+        return MaterialApp.router(
+          routerConfig: AppRouter.router,
+          title: 'Monumental Habits',
+          theme: ThemeData.light().copyWith(
+            scaffoldBackgroundColor: AppColors.scaffoldColor,
+            textTheme: GoogleFonts.manropeTextTheme(
+              ThemeData.light().textTheme,
+            ),
+          ),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
