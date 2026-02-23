54d309084d28f96d0752cc1f761187909699219f
HazemAttia7 <lolhazem41@gmail.com>
2026-02-23 22:36:15 +0200

    feat(add-new-habit) : message popup done

--- lib/core/widgets/custom_circle_icon_button.dart
+++ lib/core/widgets/custom_circle_icon_button.dart
@@ -5,23 +5,32 @@ import 'package:pixel_true_app/core/utils/app_colors.dart';
 class CustomCircleIconButton extends StatelessWidget {
   final VoidCallback onTap;
   final IconData icon;
+  final double? iconSize;
+  final EdgeInsetsGeometry? padding;
   const CustomCircleIconButton({
     super.key,
     required this.onTap,
     required this.icon,
+    this.iconSize,
+    this.padding,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return InkWell(
+      borderRadius: BorderRadius.circular(9999.r),
       onTap: onTap,
       child: Container(
-        padding: EdgeInsets.all(11.sp),
+        padding: padding ?? EdgeInsets.all(11.sp),
         foregroundDecoration: BoxDecoration(
           shape: BoxShape.circle,
           color: AppColors.primaryColor.withValues(alpha: .1),
         ),
-        child: Icon(icon, color: AppColors.primaryColor, size: 22.sp),
+        child: Icon(
+          icon,
+          color: AppColors.primaryColor,
+          size: iconSize ?? 22.sp,
+        ),
       ),
     );
   }
