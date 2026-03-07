e29c00420a65a817e3f892dd8ee17e10f3dd5ce5
HazemAttia7 <lolhazem41@gmail.com>
2026-03-07 17:01:27 +0200

    chore : edit IconButtonWithShadow radius + change month actions icons

--- lib/features/home/presentation/views/widgets/habit_analysis_widgets/month_actions.dart
+++ lib/features/home/presentation/views/widgets/habit_analysis_widgets/month_actions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pixel_true_app/core/utils/app_colors.dart';
 import 'package:pixel_true_app/core/utils/app_styles.dart';
 import 'package:pixel_true_app/features/home/presentation/views/widgets/habit_analysis_widgets/icon_button_with_shadow.dart';
@@ -19,13 +20,13 @@ class MonthActions extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButtonWithShadow(
-          icon: Icons.arrow_back_ios,
+          icon: FontAwesomeIcons.angleLeft,
           color: AppColors.primaryColor,
           onTap: onBackTap,
         ),
         Text(month, style: AppStyles.textStyle16),
         IconButtonWithShadow(
-          icon: Icons.arrow_forward_ios,
+          icon: FontAwesomeIcons.angleRight,
           color: AppColors.primaryColor,
           onTap: onNextTap,
         ),
