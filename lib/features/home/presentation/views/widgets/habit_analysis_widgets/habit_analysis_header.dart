a151c169fee4c6ec6090e7bb192298fbe0646cd7
HazemAttia7 <lolhazem41@gmail.com>
2026-03-08 00:32:46 +0200

    style(habit-analysis) : make header buttons depends on theme color

--- lib/features/home/presentation/views/widgets/habit_analysis_widgets/habit_analysis_header.dart
+++ lib/features/home/presentation/views/widgets/habit_analysis_widgets/habit_analysis_header.dart
@@ -4,21 +4,31 @@ import 'package:pixel_true_app/core/utils/app_styles.dart';
 import 'package:pixel_true_app/core/widgets/custom_icon_button.dart';
 
 class HabitAnalysisHeader extends StatelessWidget {
-  const HabitAnalysisHeader({super.key});
+  final Color themeColor;
+  const HabitAnalysisHeader({super.key, required this.themeColor});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        CustomIconButton(onTap: () {
-          Navigator.pop(context);
-        }, icon: FontAwesomeIcons.arrowLeft),
+        CustomIconButton(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          icon: FontAwesomeIcons.arrowLeft,
+          iconColor: themeColor,
+          backColor: themeColor.withValues(alpha: .1),
+        ),
         Text("Habit Analysis", style: AppStyles.textStyle18),
-        CustomIconButton(onTap: () {
-          // TODO : Edit Habit --> go to add new habit view but in edit mode
-        }, icon: FontAwesomeIcons.pencil),
-        
+        CustomIconButton(
+          onTap: () {
+            // TODO : Edit Habit --> go to add new habit view but in edit mode
+          },
+          icon: FontAwesomeIcons.pencil,
+          iconColor: themeColor,
+          backColor: themeColor.withValues(alpha: .1),
+        ),
       ],
     );
   }
