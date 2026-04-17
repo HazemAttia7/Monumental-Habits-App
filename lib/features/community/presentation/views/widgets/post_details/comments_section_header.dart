ea6c57f3dc32cbbcaada156d65cf24bc0672748e
HazemAttia7 <lolhazem41@gmail.com>
2026-04-17 15:25:54 +0200

    style(community) : add filtering capability in comments section header

--- lib/features/community/presentation/views/widgets/post_details/comments_section_header.dart
+++ lib/features/community/presentation/views/widgets/post_details/comments_section_header.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pixel_true_app/core/utils/app_colors.dart';
 import 'package:pixel_true_app/core/utils/app_styles.dart';
+import 'package:pixel_true_app/core/widgets/custom_clickable_text.dart';
 
 class CommentsSectionHeader extends StatelessWidget {
   const CommentsSectionHeader({super.key});
@@ -16,12 +18,13 @@ class CommentsSectionHeader extends StatelessWidget {
             color: AppColors.secondaryColor.withValues(alpha: .8),
           ),
         ),
-        Text(
-          "Newest First",
-          style: AppStyles.textStyle14.copyWith(
-            fontWeight: FontWeight.w600,
-            color: Color.lerp(AppColors.primaryColor, Colors.black, .25),
-          ),
+        CustomClickableText(
+          text: "Newest First",
+          fontSize: 14.sp,
+          textColor: Color.lerp(AppColors.primaryColor, Colors.black, .25),
+          onTap: () {
+            // TODO : change to oldest first - most liked
+          },
         ),
       ],
     );
