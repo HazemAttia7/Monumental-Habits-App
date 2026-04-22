b4bcb6885925722e76b58d1fc8346d86ff53fe67
HazemAttia7 <lolhazem41@gmail.com>
2026-04-22 20:05:20 +0200

    fix(post-details) : AddCommentPlaceholder layout issue

--- lib/features/community/presentation/views/widgets/post_details/post_details_view_body.dart
+++ lib/features/community/presentation/views/widgets/post_details/post_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:pixel_true_app/core/utils/constants.dart';
 import 'package:pixel_true_app/core/widgets/default_view_header.dart';
+import 'package:pixel_true_app/features/community/presentation/views/widgets/post_details/add_comment_placeholder.dart';
 import 'package:pixel_true_app/features/community/presentation/views/widgets/post_details/comments_section_header.dart';
 import 'package:pixel_true_app/features/community/presentation/views/widgets/post_details/comments_sliver_list.dart';
 import 'package:pixel_true_app/features/community/presentation/views/widgets/post_details/post_section.dart';
@@ -31,6 +32,11 @@ class PostDetailsViewBody extends StatelessWidget {
             SliverPadding(
               padding: EdgeInsets.only(bottom: 18.h),
               sliver: const SliverToBoxAdapter(child: CommentsSectionHeader()),
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [const AddCommentPlaceholder(), Gap(12.h)],
+              ),
             ),
             const CommentsSliverList(),
             SliverToBoxAdapter(child: Gap(40.h)),
